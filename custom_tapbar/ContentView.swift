829e24131be53e38defc28f3f5e4f4e41e4ddafd
l1terally <literallyuriy@gmail.com>
2023-01-01 19:19:02 +0800

    Initial Commit

---
+++ custom_tapbar/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  custom_tapbar
+//
+//  Created by Юрий  Лизунов on 01/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ custom_tapbar/custom_tapbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  custom_tapbarApp.swift
+//  custom_tapbar
+//
+//  Created by Юрий  Лизунов on 01/01/23.
+//
+
+import SwiftUI
+
+@main
+struct custom_tapbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
